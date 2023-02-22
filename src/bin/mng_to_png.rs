5591a444a76bf3a6a5943e510098f6bd02e0a568
John Lyon-Smith <john@lyon-smith.org>
2023-02-22 13:49:44 -0800

    'Initial commit'

---
+++ src/bin/mng_to_png.rs
@@ -0,0 +1,32 @@
+use colored::Colorize;
+use core::fmt::Arguments;
+use mng_to_png::{error, MngToPngLog, MngToPngTool};
+
+struct MngToPngLogger;
+
+impl MngToPngLogger {
+    fn new() -> MngToPngLogger {
+        MngToPngLogger {}
+    }
+}
+
+impl MngToPngLog for MngToPngLogger {
+    fn output(self: &Self, args: Arguments) {
+        println!("{}", args);
+    }
+    fn warning(self: &Self, args: Arguments) {
+        eprintln!("{}", format!("warning: {}", args).yellow());
+    }
+    fn error(self: &Self, args: Arguments) {
+        eprintln!("{}", format!("error: {}", args).red());
+    }
+}
+
+fn main() {
+    let logger = MngToPngLogger::new();
+
+    if let Err(error) = MngToPngTool::new(&logger).run(std::env::args_os()) {
+        error!(logger, "{}", error);
+        std::process::exit(1);
+    }
+}
